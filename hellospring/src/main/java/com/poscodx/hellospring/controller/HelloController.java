4ef1b610f37acd41e2f8beb958e0e203c61260f1
kimdongjooon <ehdwns6745@naver.com>
2023-09-14 15:48:31 +0900

    spring RequestMapping 방법.

--- hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -1,5 +1,11 @@
 package com.poscodx.hellospring.controller;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,10 +40,29 @@ public class HelloController {
 		model.addAttribute("name",n);
 		return "/WEB-INF/views/hello3.jsp";
 	}
+	
 	@ResponseBody
 	@RequestMapping("/hello5")
 	public String hello5() {
-		return "<h1>Hello5 Spring</h1>";
+		return "<h1>Hello5 Spring 안녕.</h1>";
+	}
+	
+	// 리다이렉트. 내가 작성한 mapping url만 작성가능. 
+	@RequestMapping("/hello6")
+	public String hello6() {
+		return "redirect:/hello";
+	}
+	
+	// 스프링 쓰는데 굳이 servlet기술 사용할 필요없음. 사용은 가능함.
+	@RequestMapping("/hello7")
+	public void hello7(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Writer out) throws IOException{
+		
+		String name = request.getParameter("n");
+		out.write("hello " + name);
+		
 	}
 	
 	
